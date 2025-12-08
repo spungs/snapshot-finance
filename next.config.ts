5d3e6c7bc2e096c8e479877b748e6e0778bb71c2
spungs <spungs@feelanet.com>
2025-12-08 15:30:35 +0900

    feat: add GitHub Actions CI workflow and configure Next.js for static export with base path.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/snapshot-finance',
+  assetPrefix: '/snapshot-finance/',
 };
 
 export default nextConfig;
